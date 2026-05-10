068d39048df2d81b133799367cdcad945f3d075a
JotaSxS <jpseixas2001@gmail.com>
2026-05-10 13:04:15 -0300

    rota pedidos - login

--- schemas.py
+++ schemas.py
@@ -10,3 +10,16 @@ class UsuarioSchema(BaseModel):
 
     class Config:
         from_atributes = True
+
+class PedidoSchema(BaseModel):
+    usuario: int
+
+    class Config:
+        from_atributes = True # Permite criar um schema a partir de um objeto ORM, ou seja, a partir de uma classe do SQLAlchemy.
+
+class LoginSchema(BaseModel):
+    email: str
+    senha: str
+
+    class Config:
+        from_atributes = True
